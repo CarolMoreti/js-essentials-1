2f263b3e6fac44e3e206e5dd0762f5e6dd00f1b1
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-09-15 13:23:56 -0300

    Update exercicio 2.js

--- m2/m2.1/exercicio 2.js
+++ m2/m2.1/exercicio 2.js
@@ -1,1 +1,15 @@
+let valorNulo = null;
+let valorIndefinido = undefined;
 
+console.log(`Tipo de null: ${typeof valorNulo}`); // Retorna "object" - isso é um comportamento conhecido do JavaScript.
+console.log(`Tipo de undefined: ${typeof valorIndefinido}`);
+
+let meuObjeto = {};
+let meuArray = [];
+
+console.log(`Tipo de um objeto: ${typeof meuObjeto}`);
+console.log(`Tipo de um array: ${typeof meuArray}`); // Retorna "object" - arrays são considerados objetos em JavaScript.
+
+let meuBigInt = 9007199254740991n;
+
+console.log(`Tipo de um BigInt: ${typeof meuBigInt}`);
