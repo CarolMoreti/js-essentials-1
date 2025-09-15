4d669e9719f4ccad9969aad2cf5408a4be0a9206
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-09-15 13:26:05 -0300

    Create exercicio 5.js

---
+++ m2/m2.1/exercicio 5.js
@@ -0,0 +1,5 @@
+let resultado = "A idade é: " + 25; // O número é convertido implicitamente para string.
+console.log(`Resultado da operação: ${resultado}`);
+let stringNumerica = "10";
+let numeroConvertido = +stringNumerica; // O operador unário '+' converte a string para número.
+console.log(`Resultado da conversão: ${numeroConvertido}, Tipo: ${typeof numeroConvertido}`);
