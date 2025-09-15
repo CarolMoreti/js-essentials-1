916dc651b08bbbec53b02842d2653407867dfae8
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-09-15 13:27:46 -0300

    Create exercicio 10.js

---
+++ m2/m2.1/exercicio 10.js
@@ -0,0 +1,6 @@
+let num = 5;
+let str = "10";
+let resultado = num + str; // O número é convertido para string e concatenado.
+
+console.log(`Operação: ${num} + "${str}"`);
+console.log(`Resultado: ${resultado}, Tipo: ${typeof resultado}`); // O resultado é a string "510"
