9befecdb098f8f7ec5460223447bc71af8530047
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-09-15 13:23:08 -0300

    Update exercicio 1.js

--- m2/m2.1/exercicio 1.js
+++ m2/m2.1/exercicio 1.js
@@ -5,3 +5,8 @@ let meuBooleano = true;
 console.log(`Valor: ${minhaString}, Tipo: ${typeof minhaString}`);
 console.log(`Valor: ${meuNumero}, Tipo: ${typeof meuNumero}`);
 console.log(`Valor: ${meuBooleano}, Tipo: ${typeof meuBooleano}`);
+
+let numeroParaString = 123;
+let stringConvertida = String(numeroParaString);
+
+console.log(`Resultado da conversão: ${stringConvertida}, Tipo: ${typeof stringConvertida}`);
