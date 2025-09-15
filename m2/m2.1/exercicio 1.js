a21c9b4842fffd6b3e6e0043c235500ea08df3fe
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-09-15 13:22:16 -0300

    Create exercicio 1.js

---
+++ m2/m2.1/exercicio 1.js
@@ -0,0 +1,7 @@
+let minhaString = "Olá, mundo!";
+let meuNumero = 42;
+let meuBooleano = true;
+
+console.log(`Valor: ${minhaString}, Tipo: ${typeof minhaString}`);
+console.log(`Valor: ${meuNumero}, Tipo: ${typeof meuNumero}`);
+console.log(`Valor: ${meuBooleano}, Tipo: ${typeof meuBooleano}`);
