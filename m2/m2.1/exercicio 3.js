cdfcb20434a83f493b01c4a6efe720a111c04454
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-09-15 13:24:28 -0300

    Create exercicio 3.js

---
+++ m2/m2.1/exercicio 3.js
@@ -0,0 +1,19 @@
+const tiposPrimitivos = ["string", "number", "boolean", "undefined", "null", "symbol", "bigint"];
+
+console.log(tiposPrimitivos);
+
+let tipoString = "Texto";
+let tipoNumero = 100;
+let tipoBooleano = false;
+let tipoNulo = null;
+let tipoIndefinido = undefined;
+let tipoSymbol = Symbol("id");
+let tipoBigInt = 1234567890123456789012345678901234567890n;
+
+console.log(`string: ${tipoString}`);
+console.log(`number: ${tipoNumero}`);
+console.log(`boolean: ${tipoBooleano}`);
+console.log(`null: ${tipoNulo}`);
+console.log(`undefined: ${tipoIndefinido}`);
+console.log(`symbol: ${String(tipoSymbol)}`); // Symbol precisa ser convertido para string para ser exibido.
+console.log(`bigint: ${tipoBigInt}`);
